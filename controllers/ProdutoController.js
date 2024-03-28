b5a49ac874373460fe819c6b7e8f6ae2a473aa59
ricardinszz <ricardo.privado5050@gmail.com>
2024-03-27 21:21:39 -0300

    commit 5

---
+++ controllers/ProdutoController.js
@@ -0,0 +1,37 @@
+const Produto = require("../models/Produto")
+
+const ProdutoControlller = {
+    getALL: async(req, res) => {
+        res.json( await Produto.find() )
+    },
+    get: async(req, res) => {
+        try {
+            res.json(await Produto.findById(req.params.id))    
+        } catch (error) {
+            res.status(404),json({erro: 'Registro não encontrado'})
+        }
+    },
+    create: async(req, res) => {
+        try {
+            res.json( await Produto.create(req.body) )
+        } catch (error) {
+            res.status(400),json(error)
+        }
+    },
+    update: async(req, res) => {
+        try {
+            res.json( await Produto.findByIdAndUpdate(req.params.id, req.body) ) 
+        } catch (error) {
+            res.status(404),json({erro: 'Registro não encontrado'})
+        }
+    },
+    delete: async(req, res) => {
+        try {
+            res.json(await Produto.findByIdAndDelete(req.params.id))  
+        } catch (error) {
+            res.status(404),json({erro: 'Registro não encontrado'})
+        }
+    },
+}
+
+module.exports = ProdutoControlller
--- routes/routes.js
+++ routes/routes.js
@@ -1,27 +1,19 @@
 const express = require('express')
+const ProdutoControlller = require('../controllers/ProdutoController')
 const Produto = require('../models/Produto')
 const router = express.Router()
 
 router.get('/', function(req, res){
     res.json({})
 })
-router.get('/produtos', async function(req, res){
-    res.json( await Produto.find() )
+router.get('/produtos', (req, res) => ProdutoControlller.getALL(req, res))
 
-})
-router.post('/produtos', async function(req, res){
-    res.json( await Produto.create(req.body) )
-})
-router.get('/produtos/:id', async function(req, res){
-    res.json( await Produto.findById(req.params.id) )
-})
+router.post('/produtos', (req, res) => ProdutoControlller.create(req, res))
 
-router.delete('/produtos/:id', async function(req, res){
-    res.json( await Produto.findByIdAndDelete(req.params.id) )
-})
+router.get('/produtos/:id', (req, res) => ProdutoControlller.get(req, res))
 
-router.put('/produtos/:id', async function(req, res){
-    res.json( await Produto.findByIdAndUpdate(req.params.id, req.body) )
-})
+router.put('/produtos/:id', (req, res) => ProdutoControlller.update(req, res))
+
+router.delete('/produtos/:id', (req, res) => ProdutoControlller.delete(req, res))
 
 module.exports = router
