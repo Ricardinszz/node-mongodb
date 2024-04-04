ae2e1feb1664c1689a7649fb54a136badf62e765
ricardinszz <ricardo.privado5050@gmail.com>
2024-04-03 22:02:56 -0300

    commit test

--- models/Comanda.js
+++ models/Comanda.js
@@ -1,10 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const schema = mongoose.Schema({
     nome: String,
-})
+    
+    data: {
+        type: Date,
+        required: true,
+        set: (value) => {
+            const [dia, mes, ano] = value.split('/');
+            return `${ano}-${mes}-${dia}`;
+        }
+    }
+});
 
-const Comanda = mongoose.model('Comanda', schema)
+const Comanda = mongoose.model('Comanda', schema);
 
-module.exports = Comanda
-
+module.exports = Comanda;
--- models/produto.js
+++ models/produto.js
@@ -1,11 +1,34 @@
 const mongoose = require('mongoose')
 
 const schema = mongoose.Schema({
-    nome: String,
-    preco: Number,
-    tamanho: String,
-    tipo: String,
-    ingredientes: []
+    nome: { 
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 100,
+        trim: true,
+    },
+    preco: {
+        type: Number,
+        required: true,
+        min: [0, 'Mais facil'],
+        match: /^\d+(\d{1,2})?$/,
+    },
+    tamanho: {
+        type: String,
+        enum: ['Pequeno', 'Médio', 'Grande'],
+        default: 'Médio'
+    },
+    tipo: {
+        type: String,
+        enum: ['Bebidas', 'Pizzas', 'Sucos'],
+        default: 'Médio'
+    },
+    ingredientes: [{
+        type: String,
+        enum: ['calabresa', 'queijo', 'massa'],
+        required: true
+    }]
 })
 
 const Produto = mongoose.model('Produto', schema)
